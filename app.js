e3c1e622daae68de19b43e69c7077be3530c3df9
isabelleadrury <isabelledrury38@gmail.com>
2025-10-15 14:04:42 -0400

    added employee-directory

---
+++ app.js
@@ -0,0 +1,7 @@
+const employees = [
+  { id: 1, name: "Alice" },
+  { id: 2, name: "Bob" },
+  { id: 3, name: "Cameron" },
+];
+
+module.exports = { employees };
--- server.js
+++ server.js
@@ -1,5 +1,26 @@
+const express = require("express");
+const app = express();
+const { employees } = require("./app");
+
 import app from "#app";
 const PORT = 3000;
+
+app.get("/", (request, response) => {
+  response.send("Hello, Employees!");
+});
+
+app.get("/employees/:id", (req, res) => {
+  console.log(typeof req.params.id);
+  const employeeId = Number(req.params.id);
+  const foundEmployee = employees.find(
+    (employee) => employee.id === employeeId
+  );
+  if (!foundEmployee) {
+    return res.status(404).json({ error: "Employee not found" });
+  }
+  res.json(foundEmployee);
+});
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
